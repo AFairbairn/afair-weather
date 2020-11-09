3448b7248402467331df6ac946305e852b6ea12c
Andrew Fairbairn <afairbairn85@googlemail.com>
2020-11-09 16:51:15 +0100

    fixed missspelling

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fetch = require("node-fetch");
 require('dotenv').config();
 const opencage = require('opencage-api-client');
 const app = express();
-const PORT = proces.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log("Server Started"));
 app.use(express.static("public"));
